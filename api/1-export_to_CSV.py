c901d4ce3ae12dbcf45b6dddc18b2ad9e82f0304
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-22 16:26:02 +0200

    Task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+This module extracts data extends from file1
+and save data to a csv file
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    import csv
+
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    try:
+        int(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    # URLs
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+    # Get user name
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print(f"Employee with ID {employee_id} not found.")
+        sys.exit(1)
+
+    employee_name = user_response.json().get('name')
+
+    # Get todos
+    response = requests.get(todos_url, params={'userId': employee_id})
+    if response.status_code == 200:
+        todos = response.json()
+        with open(f'{employee_id}.csv', 'w', newline='') as file:
+            fieldnames = ['employee Id',
+                          'employee name',
+                          'Task Completed Status',
+                          'Task Title'
+                          ]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            for tasks in todos:
+                writer.writerow({
+                    'employee Id': employee_id,
+                    'employee name': employee_name,
+                    'Task Completed Status': tasks.get('completed'),
+                    'Task Title': tasks.get('title')
+                }
+                )
+    else:
+        print(f"Failed to retrieve todos for employee {employee_id}")
