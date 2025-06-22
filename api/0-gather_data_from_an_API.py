93613623f8141e2ddac3ea33822c55dbe20333eb
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-22 14:42:31 +0200

    Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,30 +2,30 @@
 # This module extracts a user's todo list based on their ID
 
 import requests
+import sys
 
-# Define API URL and parameters
-api_url = "https://jsonplaceholder.typicode.com/todos"
-parameters = {
-    'userId': 1
-}
+if __name__ == "__main__":
+    # Define API URL and parameters
+    api_url = "https://jsonplaceholder.typicode.com/todos"
+    employeeId = sys.argv[1]
+    parameters = {
+        'userId': employeeId
+        }
+    # Fetch data from API
+    response = requests.get(api_url, params=parameters)
+    if response.status_code == 200:
+        todos = response.json()
+        # Count completed tasks
+        completed_tasks = [task for task in todos if task.get('completed')]
+        count_done = len(completed_tasks)
+        nbr_tasks = len(todos)
 
-# Fetch data from API
-response = requests.get(api_url, params=parameters)
-
-if response.status_code == 200:
-    todos = response.json()
-
-    # Count completed tasks
-    completed_tasks = [task for task in todos if task['completed']]
-    count_done = len(completed_tasks)
-    nbr_tasks = len(todos)
-
-    # Display the report
-    print(
-        f"Employee Patricia Leshler is done with tasks "
-        f"({count_done}/{nbr_tasks}):"
-    )
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
-else:
-    print(f"Failed to retrieve data: Status code {response.status_code}")
+        # Display the report
+        print(
+            f"Employee {employeeId} is done with tasks "
+            f"({count_done}/{nbr_tasks}):"
+        )
+        for task in completed_tasks:
+            print(f"\t {task.get('title')}")
+    else:
+        print(f"Failed to retrieve data: Status code {response.status_code}")
