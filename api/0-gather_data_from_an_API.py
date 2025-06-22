98fe029b65248abeeeb1260a2b824765ef6df109
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-22 09:20:23 +0200

    Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,16 +11,16 @@ parameter = {
     'userID': 1
 }
 
-response = requests.get(api_url, params= parameter)
+response = requests.get(api_url, params=parameter)
 
-if response.status_code == 200 :
+if response.status_code == 200:
     response_data = response.json()
 
 count_done = 0
 
 # count the number of completed tasks
 for completion in response_data:
-    if completion['completed'] == True:
+    if completion['completed'] is True:
         count_done += 1
 
 # number of tasks
@@ -29,5 +29,5 @@ nbr_tasks = len(response_data)
 # Display the report
 print(f"Patricia Leshler is done with tasks({count_done}/{nbr_tasks})")
 for completed_tasks in response_data:
-    if completed_tasks['completed'] == True:
+    if completed_tasks['completed'] is True:
         print(f'\t {completed_tasks['title']}')
